b2b5cbce53d7835bd4b3e99c716101bbb6108fa1
Leydis <leydisfh@gmail.com>
2024-05-15 12:15:15 -0500

    Refactor error handling in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -50,7 +50,7 @@ const toggleImportanceOf = (id) =>{
   .then(returnedNote =>{
     setNotes(notes.map(note => note.id !== id ? note : returnedNote))
   })
-  .catch(error =>{
+  .catch(() =>{
     setErrorMessage(`Note  ${note.content}  was already removed from server`  )
     setTimeout(()=>{
       setErrorMessage(null)
